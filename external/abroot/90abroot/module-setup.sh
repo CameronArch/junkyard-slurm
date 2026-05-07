966e3ddad16525a15aa2b30571176f140d6c3ee7
CameronArch <cameronarch598@gmail.com>
2026-05-07 14:08:28 -0700

    change priority only has first fix attempt

--- external/abroot/90abroot/module-setup.sh
+++ external/abroot/90abroot/module-setup.sh
@@ -11,6 +11,6 @@ depends() {
 
 install() {
     inst_hook cmdline 99 "$moddir/parse-abroot.sh" # 99 priority to run after cmdline hooks from other modules
-    inst_hook pre-mount 01 "$moddir/patch-sysroot-mount.sh" # 01 priority to run before sysroot-mount runs
+    inst_hook pre-mount 90 "$moddir/patch-sysroot-mount.sh" # 90 priority to run before sysroot-mount runs
 }
 
